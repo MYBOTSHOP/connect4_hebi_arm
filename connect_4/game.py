b0a005abc00d730b978ab67291c03e63107af34c
Daniel Kottlarz <40395615+MYBOTSHOP@users.noreply.github.com>
2020-10-13 09:15:32 +0200

    Add files via upload
    Adding source files

---
+++ connect_4/game.py
@@ -0,0 +1,263 @@
+# =============================================================================
+# 
+# This module contains the :class:`Board` and the :class:`Game` class.
+# 
+# =============================================================================
+
+# Note: some (great) implementation ideas were inspired by Patrick Westerhoff:
+# https://gist.github.com/poke/6934842
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+from itertools import chain
+from itertools import groupby
+import sys
+sys.path.append("..")
+from robot_controller_utils import KThread, Hebi3dofArm, StateMachine
+class Board:
+# =============================================================================
+#     The Board class.
+# 
+#     Args:
+#         n_rows(int): The number of rows of the board.
+#         n_cols(int): The number of columns of the board.
+# 
+#     Attributes:
+#         n_rows(int): The number of rows of the board.
+#         n_cols(int): The number of columns of the board.
+#         grid(list of list): The proper board, represented as a ``n_rows`` *
+#             ``n_cols`` matrix.
+#     
+# =============================================================================
+
+    EMPTY = '.'
+
+    def __init__(self, n_rows, n_cols):
+
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.grid = [[self.EMPTY] * n_cols for _ in range(n_rows)]
+
+    def insert(self, col, coin):
+# =============================================================================
+#         Insert a piece in given column.
+# 
+#         Args:
+#             col(int): The column.
+#             coin(str): The coin to insert.
+# 
+#         Returns:
+#             row(int): The row where the coin was inserted.
+#         Raises:
+#             ValueError: if ``col`` is full or is out of range.
+#         
+# =============================================================================
+
+        if col < 0 or col >= self.n_cols:
+            raise ValueError('Invalid column ' + str(col) + '.')
+
+        try:
+            r = next(r for r in range(self.n_rows - 1, -1, -1)
+                     if self.grid[r][col] == self.EMPTY)
+            self.grid[r][col] = coin
+        except StopIteration:
+            raise ValueError('Column ' + str(col) + ' is already full.')
+
+        return r
+
+    def is_free(self, col):
+# =============================================================================
+#         Check if a coin can be inserted in given column.
+# 
+#         Args:
+#             col(int): The column
+# 
+#         Returns:
+#             ``True`` if the column is free, else ``False``.
+#         
+# =============================================================================
+
+        return self.grid[0][col] == self.EMPTY
+
+    def free_columns(self):
+# =============================================================================
+#         Generator function to iterate over all free columns
+# 
+#         Returns:
+#             All free columns 
+# =============================================================================
+        return (col for col in range(self.n_cols) if self.is_free(col))
+
+    def is_full(self):
+# =============================================================================
+#         Check if the board is full.
+# 
+#         Returns:
+#             ``True`` if the board is full, else ``False``.  
+# =============================================================================
+
+        return all(not self.is_free(col) for col in range(self.n_cols))
+
+    def all_sequences(self, to_win=1):
+# =============================================================================
+#         Generator function to iterate over all sequences of the board.
+# 
+#         A sequence is either a row, a column or a diagonal.
+# 
+#         Args:
+#             to_win(int, optional): The number of successive coins needed to win
+#                 a game. Only diagonals with a length greater or equal to
+#                 ``to_win`` will be yielded.
+# 
+#         Returns:
+#             All sequences.
+# =============================================================================
+
+        def diagonals():
+# =============================================================================
+#             Generator function to iterate over all the diagonals.
+# =============================================================================
+            # Note : we actually yield a list and not just a generator so that
+            # diagonals can be iterated multiple times.
+            start = to_win - 1
+            end = self.n_rows + self.n_cols - to_win
+            pos_diag_indices = (((r - c, c) for c in range(self.n_cols))
+                                for r in range(start, end))
+            start = to_win - self.n_cols
+            end = self.n_rows - to_win + 1
+            neg_diag_indices = (((r + c, c) for c in range(self.n_cols))
+                                for r in range(start, end))
+            for d in chain(pos_diag_indices, neg_diag_indices):
+                yield [self.grid[i][j] for (i, j) in d
+                       if 0 <= i < self.n_rows and
+                       0 <= j < self.n_cols]
+        rows = self.grid
+        columns = zip(*self.grid)
+        return chain(rows, columns, diagonals())
+    def __str__(self):
+        s = ' '.join('{0:2s}'.format(str(i + 1))
+                     for i in range(self.n_cols)) + '\n'
+        s += '\n'.join('  '.join(cell for cell in row)
+                       for row in self.grid)
+        return s
+
+class Game:
+# =============================================================================
+#     A basic engine for the connect4 game.
+# 
+#     Args:
+#         players(tuple of :class:`Player <connect4.player.Player>`): The two players. The first one is first to play.
+#         n_rows(int): The number of rows of the board. Default is ``6``.
+#         n_cols(int): The number of columns of the board. Default is ``7``.
+#         to_win(int): The number of aligned pieces required to win the game.
+#             Default is ``4``.
+# 
+#     Attributes:
+#         player1(:class:`Player <connect4.player.Player>`): The first
+#             player.
+#         player2(:class:`Player <connect4.player.Player>`): The second
+#             player.
+#         board(:class:`Board`): The board.
+#         to_win(int): The number of aligned pieces required to win the game.  
+# =============================================================================
+    def __init__(self, players, n_rows=6, n_cols=7, to_win=4):
+        self.board = Board(n_rows, n_cols)
+        self.player1, self.player2 = players
+        self.player1.opponent = self.player2
+        self.player2.opponent = self.player1
+        self.to_win = self.player1.to_win = self.player2.to_win = to_win
+
+        self.ssm = StateMachine()
+
+        if self.player1.coin == self.player2.coin:
+            raise ValueError('Both players have the same coin.')
+    def check_winner(self):
+# =============================================================================
+#         Check if there's a winner at the current game state.
+# 
+#         Returns:
+#             :class:`Player <connect4.player.Player>`: The winner.  If no
+#             player has won yet, ``None`` is returned.  
+# =============================================================================
+        # for every line, column and diag, check if there are at least 'to_win'
+        # pieces of the same color that are aligned.
+        for sequence in self.board.all_sequences():
+            for coin, group in groupby(sequence):
+                if (coin != self.board.EMPTY and
+                   len(list(group)) >= self.to_win):
+                    return (self.player1 if coin == self.player1.coin else
+                            self.player2)
+        return None
+    def run(self):
+# =============================================================================
+#         Run a game session between the two players.
+# 
+#         Returns:
+#             :class:`Player <connect4.player.Player>`: The winner.  
+# =============================================================================
+
+        winner = None
+        current_player = self.player1
+        while winner is None and not self.board.is_full():
+            print(self.board)
+            col = current_player.play(self.board)
+            print('Player {0} plays in column {1}.'.format(
+                  current_player, col + 1))
+            self.board.insert(col, current_player.coin)
+            winner = self.check_winner()
+            current_player = (self.player1 if current_player == self.player2
+                              else self.player2)
+            print()
+        print(self.board)
+        if winner is not None:
+            print('Player {0} won the game!'.format(winner))
+        else:
+            print("There's no winner. You're both LOSERS.")
+        return winner
+
+    def run_robot(self):
+# =============================================================================
+#         Run a game session between the two players.
+# 
+#         Returns:
+#             :class:`Player <connect4.player.Player>`: The winner.  
+# =============================================================================
+
+        winner = None
+        current_player = self.player1
+
+        self.ssm.arm.initialize_arm()
+        thread = KThread(target=self.ssm.arm.handle_joints,)
+        thread.start()
+        self.ssm.go_home()
+
+        while winner is None and not self.board.is_full():
+            print(self.board)
+            col = current_player.play(self.board)
+            if current_player ==  self.player2:
+                if col == 0:
+                    self.ssm.state_transition("C1")
+                elif col == 1:
+                    self.ssm.state_transition("C2")
+                elif col == 2:
+                    self.ssm.state_transition("C3")
+                elif col == 3:
+                    self.ssm.state_transition("C4")
+                elif col == 4:
+                    self.ssm.state_transition("C5")
+                elif col == 5:
+                    self.ssm.state_transition("C6")
+                elif col == 6:
+                    self.ssm.state_transition("C7")
+            print('Player {0} plays in column {1}.'.format(
+                  current_player, col + 1))
+            self.board.insert(col, current_player.coin)
+            winner = self.check_winner()
+            current_player = (self.player1 if current_player == self.player2
+                              else self.player2)
+            print()
+        print(self.board)
+        if winner is not None:
+            print('Player {0} won the game!'.format(winner))
+        else:
+            print("There's no winner. You're both LOSERS.")
+        return winner
--- connect_4/player.py
+++ connect_4/player.py
@@ -1,1 +1,277 @@
+# =============================================================================
+# 
+# This module contains the :class:`Player` class and all derived player classes.
+# 
+# =============================================================================
+from __future__ import absolute_import, division, print_function, unicode_literals
+import random
+from itertools import groupby
+try:
+    from itertools import zip_longest
+except ImportError:
+    from itertools import izip_longest as zip_longest  # due to error in Python 2
+class Player:
+# =============================================================================
+#     The Player base class.
+# 
+#     Args:
+#         coin(str): The coin representing the user.  
+# =============================================================================
 
+    def __init__(self, coin):
+        self.coin = coin
+        # Players (may) need to know their opponent and the number of
+        # successive coins needed to win the game.
+        self.opponent = None
+        self.to_win = None  
+
+    def play(self, board):
+# =============================================================================
+#         Choose a random column to play on.
+# 
+#         Args:
+#             board(:class:`Board <connect4.game.Board>`): The current board.
+# 
+#         Returns:
+#             (int): The column to play on.
+# =============================================================================
+        found_col = 0
+        while not found_col:
+            col = random.randint(0, board.n_cols - 1)
+            found_col = board.is_free(col)
+        return col    
+    def __str__(self):
+        return str(self.coin)
+
+class Human(Player):
+# =============================================================================
+#     A Human player.
+# 
+#     Args:
+#         coin(str): The coin representing the user.  
+# =============================================================================
+    def __init__(self, coin):
+        Player.__init__(self, coin)
+
+    def play(self, board):
+# =============================================================================
+#         Ask user where he/she wants to play.
+# 
+#         Args:
+#             board(:class:`Board <connect4.game.Board>`): The current board.
+# 
+#         Returns:
+#             (int): The column to play on.  
+# =============================================================================
+        input_ok = False
+        while not input_ok:
+            try:
+                msg = "{0}'s turn. Please choose a column:".format(self)
+                col = int(input(msg)) - 1
+                assert 0 <= col < board.n_cols
+                assert board.is_free(col)
+                input_ok = True
+            except (ValueError, AssertionError):
+                print('Invalid choice.')
+                pass
+        return col
+
+class Minimax(Player):
+# =============================================================================
+#     An AI player using the Minimax algorithm (with alpha/beta pruning).
+# 
+#     Args:
+#         coin(str): The coin representing the user.
+#         depth: The maximum depth of the minimax algorithm. Default is ``5``.
+# =============================================================================
+    
+    def __init__(self, coin, depth=5):
+
+        Player.__init__(self, coin)
+        self.depth = depth
+
+    def utility(self, board):
+# =============================================================================
+#         The utility function to evaluate the *goodness* of a board for the
+#         player.
+# 
+#         Right now, the evaluation is as follows:
+# 
+#             - If the player wins, :math:`\infty` is returned.
+#             - If he loses, :math:`-\infty` is returned.
+#             - If there is no winner, the utility value starts from :math:`0`
+#               and inscremented by :math:`l^2` for each sequence of length
+#               :math:`l` that could lead to a win for the player, and
+#               decremented by the same amount for each sequence that could lead
+#               to a win for the opponent.
+# 
+#         Args:
+#             board(:class:`Board <connect4.game.Board>`): The board to evaluate.
+# 
+#         Returns:
+#             The estimated utility of the board.
+# =============================================================================
+
+        def pred_current_next(iterable, fill_pred, fill_next):
+# =============================================================================
+#             Generator function that will yield each element of the iterable,
+#             surrounded by it's previous and next element. Default values for
+#             previous and next need to be assigned.
+#             
+# =============================================================================
+            pred_elt = fill_pred
+            for current_elt, next_elt in zip_longest(iterable, iterable[1:],
+                                                     fillvalue=fill_next):
+                yield pred_elt, current_elt, next_elt
+                pred_elt = current_elt
+
+        h = 0
+        for sequence in board.all_sequences(to_win=self.to_win):
+            grouped = [(key, list(g)) for (key, g) in groupby(sequence)]
+            for ((pred_coin, pred_group),
+                 (current_coin, current_group),
+                 (next_coin, next_group)) in pred_current_next(
+                                             grouped,
+                                             fill_pred=(None, []),
+                                             fill_next=(None, [])):
+                l = len(current_group)
+                if l >= self.to_win:
+                    if current_coin == self.coin:
+                        return float('inf')
+                    elif current_coin == self.opponent.coin:
+                        return float('-inf')
+                else:
+                    mul = (pred_coin == '.' and
+                           l + len(pred_group) >= self.to_win)
+                    mul += (next_coin == '.' and
+                            l + len(next_group) >= self.to_win)
+                    if current_coin == self.coin:
+                        h += mul * l**2
+                    elif current_coin == self.opponent.coin:
+                        h -= mul * l**2
+        # Try to force central playing
+        for row in range(1, board.n_rows // 2):
+            h += board.grid[-row][board.n_cols // 2] == self.coin
+        return h
+    def minimax(self, node, depth, alpha, beta):
+# =============================================================================
+#         Run the minimax graph exploration procedure on given node, and
+#         update the node's attributes.
+# 
+#         If the node is a terminal node (max depth is reached, some player wins
+#         or the board is full), then ``node.score`` is set using the
+#         :meth:`utility()` function.
+# 
+#         Else, :meth:`minimax` is called for all possible child nodes to compute
+#         their score. Once done, the current node score is updated (depending on
+#         the player) and the column that leads to the best score is also set.
+# 
+#         Args:
+#             node(Node): The current node.
+#             depth(int): The current depth
+#             alpha: The best value that player *self* can expect so far.
+#             beta: The best value that the opponent of player *self* can expect
+#                 so far.
+# =============================================================================
+        # Compute utility of current node: if there's a winner, we want to stop
+        # the search.
+        score = self.utility(node.board)
+        # Stop the search if the maximum depth is reached, if there's a winner
+        # or if the board is full.
+        if (depth == 0 or
+            score in (float('-inf'), float('inf')) or
+            node.board.is_full()):  # noqa
+            node.score = score
+            return
+
+        best_child = Node(board=None,
+                          player=None,
+                          col_played=None,
+                          col_to_play=None,
+                          score=(float('-inf') if node.player is self
+                                 else float('inf')),
+                          childs=[])
+
+        # For every possible move
+        for col in node.board.free_columns():
+            # Build a child node with an updated board. We'll need to delete
+            # the coin later as the same board object is shared among all
+            # nodes.
+            child_board = node.board
+            row = child_board.insert(col, node.player.coin)
+            child = Node(board=child_board,
+                         player=(self.opponent if node.player is self
+                                 else self),
+                         col_played=col,
+                         col_to_play=None,  # will be set later on
+                         score=None,  # will be set later on
+                         childs=[]  # will be set later on
+                         )
+            self.minimax(child, depth - 1, alpha, beta)
+            # Now delete the child's move from the board
+            child_board.grid[row][col] = child_board.EMPTY
+
+            # Update the best_child, alpha, beta and prune if needed.
+            if node.player is self:
+                best_child = max((best_child, child),
+                                 key=lambda x: x.score)
+                alpha = max(alpha, best_child.score)
+            else:
+                best_child = min((best_child, child),
+                                 key=lambda x: x.score)
+                beta = min(beta, best_child.score)
+            if beta <= alpha:
+                break
+        # And most importantly, set the column to play and the node score.
+        node.col_to_play = best_child.col_played
+        node.score = best_child.score
+    def play(self, board):
+# =============================================================================
+#         Choose a column to play on based on the minimax algorithm.
+# 
+#         Args:
+#             board(:class:`Board <connect4.game.Board>`): The current board.
+# 
+#         Returns:
+#             (int): The column to play on.
+# =============================================================================
+        node = Node(board=board,
+                    player=self,
+                    col_played=None,
+                    col_to_play=None,
+                    score=None,
+                    childs=[])
+        self.minimax(node, self.depth, alpha=float('-inf'), beta=float('inf'))
+        print(node)
+        return node.col_to_play
+
+class Node:
+# =============================================================================
+#     A node class for the minimax algorithm graph search.
+# 
+#     A node represents a game state.
+# 
+#     Args:
+#         board(:class:`Board <connect4.game.Board>`): The current board. For
+#             efficiency purpose, the board is actually shared among all nodes of
+#             the graph.
+#         player(:class:`Player`): The current player.
+#         col_played(int): The column played at the parent node.
+#         col_to_play(int): The column to play that will result in the best
+#             outcome.
+#         score(int): The score of the current game state.
+#         childs(list): The child nodes.
+# =============================================================================
+
+    def __init__(self, board, player, col_played, col_to_play, score, childs):
+        self.board = board
+        self.player = player
+        self.col_to_play = col_to_play
+        self.col_played = col_played
+        self.score = score
+        self.childs = childs
+    def __str__(self):
+        return '\n'.join(['col_to_play = ' + str(self.col_to_play),
+                          'col_played = ' + str(self.col_played),
+                          'score = ' + str(self.score),
+                          'n_chids = ' + str(len(self.childs))])
